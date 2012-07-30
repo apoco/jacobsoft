67d93de1600b6d437d5ae7978586b95dae5cb1ae
Jacob Page <jacob.h.page@gmail.com>
2012-07-30 01:14:57 -0700

    Added lite loader and unit tests

---
+++ Jacobsoft.Amd/Scripts/liteloader.js
@@ -0,0 +1,35 @@
+﻿(function (exports) {
+
+    circularRef = {};
+
+    modules = {};
+
+    function define(name, deps, factory) {
+        modules[name] = { deps: deps, factory: factory, exports: undefined };
+    }
+
+    function require(deps, factory) {
+        var args = [];
+        for (var i = 0; i < deps.length; i++) {
+            var module = modules[deps[i]];
+            args.push(module ? resolve(module) : undefined);
+        }
+        if (factory) {
+            return (typeof (factory) == 'function')
+                ? factory.apply(factory, args)
+                : factory;
+        }
+    }
+
+    function resolve(module) {
+        if (module.exports === undefined) {
+            module.exports = null;
+            module.exports = require(module.deps, module.factory);
+        }
+        return module.exports;
+    }
+
+    exports.define = define;
+    exports.require = require;
+
+})(window);
---
+++ Jacobsoft.Amd/Scripts/liteloader.tests.js
@@ -0,0 +1,74 @@
+﻿/// <reference path="liteloader.js" />
+
+describe('liteloader', function () {
+    it('provides a define function', function () {
+        expect(typeof(require)).toBe('function');
+    });
+
+    it('provides a require function', function () {
+        expect(typeof (require)).toBe('function');
+    });
+});
+
+describe('the define function', function () {
+
+    it('can register an object', function () {
+        var obj = { foo: 'bar' };
+        define('a', [], obj);
+        require(['a'], function (a) {
+            expect(a).toBe(obj);
+        });
+    });
+
+    it('can register a factory', function () {
+        var obj = { foo: 'bar' };
+        define('b', [], function () { return obj; });
+        require(['b'], function (b) {
+            expect(b).toBe(obj);
+        });
+    });
+
+    it('can declare dependencies', function () {
+
+        define('c', ['d'], function (d) { return d.foo; });
+        define('d', [   ], function ( ) { return { foo: 'bar' }; });
+
+        require(['c'], function (c) {
+            expect(c).toBe('bar');
+        });
+    });
+});
+
+describe('the require function', function () {
+    
+    it('can just execute a defined module', function () {
+        var hasRun = false;
+
+        define('e', [], function () { hasRun = true; });
+        expect(hasRun).toBe(false);
+
+        require(['e']);
+        expect(hasRun).toBe(true);
+    });
+
+    it('can invoke a callback', function () {
+        define('f', [], function () { return 'foo'; });
+        require(['f'], function (f) { expect(f).toBe('foo'); });
+    });
+
+    it('resolves dependencies recursively', function () {
+        define('g', ['h'], function (h) { return h.foo; });
+        define('h', ['i'], function (i) { return { foo: i.bar }; });
+        define('i', [], function () { return { bar: 'bar' }; });
+        require(['g'], function (g) { expect(g).toBe('bar'); });
+    });
+
+    it('runs factories only once', function () {
+        var object1, object2;
+        define('j', [], function () { return { foo: 'bar' }; });
+        require(['j'], function (j) { object1 = j; });
+        require(['j'], function (j) { object2 = j; });
+        expect(object1).toBe(object2);
+    });
+
+});
